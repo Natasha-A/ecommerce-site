e92d7eb242c479700133efcb8d2b7e1c57df1568
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-11-29 09:55:45 -0500

    add user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// GET Register form  
+router.get('/register', function(req, res, next){
+  res.render('register_form', {title: 'Not implemented: GET Register Form'})
+})
+
+// POST Register form 
+router.post('/register', function(req, res, next){
+  res.render('register_form', {title: 'Not implemented: POST Register Form'})
+})
+
+// GET Login form 
+router.get('/login', function(req, res, next) {
+  res.render('login_form', {title: 'Not implemented: GET Login Form'})
+})
+
+// POST Login form 
+router.post('/login', function(req, res, next) {
+  res.render('login_form', {title: 'Not implemented: POST Login Form'})
+})
+
+// GET Logout
+router.get('/logout', function(req, res) {
+  res.redirect('/login')
+})
 
 module.exports = router;
