68c99fe03c7bece74dbedd085f3fca7022f146f9
Melody Yip <melodyyip515@hotmail.com>
2022-12-02 16:44:01 -0500

    add /product/:id

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,6 @@ router.get('/', function (req, res, next) {
 
     }
     else {
-      console.log(productsArray);
       res.render('index', { title: 'Clothing Shop | List of Clothing', products: productsArray });
     }
   })
@@ -57,6 +56,7 @@ router.get('/product/:id', function(req,res) {
       res.end("404 - No this product");
     }
     else {
+      console.log(product)
       res.render('product_detail', { title: 'product_detail', product: product });
     }
   });
