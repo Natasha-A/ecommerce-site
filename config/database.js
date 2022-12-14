26adbf49e8b023a718f8b53b2ee8813865b4c9d3
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-12-14 13:45:49 -0500

    secret key added

--- config/database.js
+++ config/database.js
@@ -1,3 +1,6 @@
 require('dotenv').config()
 
-module.exports = {connectionString: 'mongodb+srv://ecommerce-site:ecommerce@cluster0.k9yvipi.mongodb.net/ecommerce'}
+module.exports = {connectionString: 'mongodb+srv://ecommerce-site:ecommerce@cluster0.k9yvipi.mongodb.net/ecommerce',
+secret: 'secret'
+
+}
